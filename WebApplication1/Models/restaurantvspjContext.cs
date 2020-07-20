1445d6dbd414e0ff1624f5022ebed90f2da6816d
Urban, Michal <Michal.Urban@tesco.com>
2020-07-20 16:23:30 +0200

    ..PW hidden

--- WebApplication1/Models/restaurantvspjContext.cs
+++ WebApplication1/Models/restaurantvspjContext.cs
@@ -29,7 +29,7 @@ namespace WebApplication1.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=tcp:vspj.database.windows.net,1433;Initial Catalog=restaurant.vspj;Persist Security Info=False;User ID=murban27;Password=Swimer01;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+                optionsBuilder.UseSqlServer("Server=tcp:vspj.database.windows.net,1433;Initial Catalog=restaurant.vspj;Persist Security Info=False;User ID=AskForUserName;Password=AskForPassword;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
             }
         }
 
